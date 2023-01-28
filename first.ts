710b8a1cbc0097defb1bdb4c3ef90b2c76227429
sazzad4677 <sazzad35-1898@diu.edu.bd>
2023-01-28 16:15:02 +0600

    ts types

--- first.ts
+++ first.ts
@@ -99,7 +99,7 @@ console.log(product);
  * Example of ENuM
  */
 enum Role {
-  ADMIN = 4 ,
+  ADMIN = 4,
   READ_ONLY,
   AUTHOR,
 }
@@ -108,3 +108,79 @@ const person = {
   age: 30,
   Permissions: Role.ADMIN,
 };
+/*
+ * Example of Union Types
+ */
+
+function combine(n1: number | string, n2: number | string) {
+  let result;
+  if (typeof n1 === "string" && typeof n2 === "string") {
+    result = n1.toString() + n2.toString();
+  }
+  if (typeof n1 === "number" && typeof n2 === "number") {
+    result = n1 + n2;
+  }
+  return result;
+}
+
+const combinedAges = combine(1, 2);
+console.log(combinedAges); //  3
+
+const combinedNames = combine("a", "b");
+/*
+ * Example of literal Types
+ */
+function literalTypes(params: "as-number" | "as-text") {
+  if (params === "as-number") {
+    return 1;
+  }
+  return (1).toString();
+}
+const convertNumber = literalTypes("as-number"); // 1
+const convertText = literalTypes("as-text"); // "1"
+
+/*
+ * Example of type aliases
+ */
+type Combinable = number | string;
+
+function typeAlias(params: Combinable) {
+  return params;
+}
+
+type User = { name: string; age: number };
+
+function greet(user: User) {
+  console.log("Hi, I am " + user.name);
+}
+
+function isOlder(user: User, checkAge: number) {
+  return checkAge > user.age;
+}
+/*
+ * Example of function return types and void
+ */
+function add(n1: number, n2: number): number {
+  return n1 + n2;
+}
+function printResult(params: number): void {
+  // void will be used as function doesn't have any returns, so instead of using undefined void will be used as function type
+  console.log("Result: " + params);
+}
+/*
+ * Example of function types
+ */
+
+let funcType: Function;
+funcType = add;
+// funcType = 5;
+let anotherWay: (a: number, b: number) => number;
+anotherWay = add;
+// anotherWay = printResult => this will throw an error
+
+/*
+ * Example of unknown types
+ */
+let userInput: unknown;
+userInput = 5;
+userInput = "hello world";
